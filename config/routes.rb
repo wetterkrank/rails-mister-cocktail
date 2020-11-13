8a12e5c49d5b5a29373fa3bd44b41fedae641ad6
Alex Antsiferov <wetterkrank@gmail.com>
2020-11-13 16:02:16 +0100

    Root to index, additional seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [ :create ]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,34 @@
 require 'json'
 require 'open-uri'
+require 'csv'
 
+# Additional bulk import from CSV
+puts "Adding some stuff into DB..."
+
+my_ingredients = []
+CSV.foreach('db/seeds/ingredients.csv') { |row| my_ingredients << row[0].strip }
+my_ingredients.each { |ing| Ingredient.create(name: ing) }
+
+puts "Done."
+exit
+
+# This is the initial seeding
 puts "Cleaning database now..."
 Cocktail.destroy_all
 Ingredient.destroy_all
 puts "Database clean ✅"
 
+# Get some ingredients to work with
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list_serialized = open(url).read
+ingredients = JSON.parse(list_serialized)
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+# Make one cocktail as well
 ice = Ingredient.create(name: 'Ice')
 vodka = Ingredient.create(name: 'Vodka')
-
 ice_vodka = Cocktail.create(name: 'Ice Vodka')
 
 dose = Dose.new(description: "50ml")
@@ -20,13 +40,5 @@ dose = Dose.new(description: "2 cubes")
 dose.ingredient = ice
 dose.cocktail = ice_vodka
 dose.save
-
-# Get some more ingredients to work with
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-list_serialized = open(url).read
-ingredients = JSON.parse(list_serialized)
-ingredients["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient["strIngredient1"])
-end
 
 puts "Done! Created #{Ingredient.count} ingredients and #{Cocktail.count} cocktails."
